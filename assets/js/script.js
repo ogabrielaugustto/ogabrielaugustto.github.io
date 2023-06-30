84a3d8bfcca7ed9b10924f33ad8e109cb1656ba9
Gabriel Augusto <89751147+ogabrielaugustto@users.noreply.github.com>
2023-06-29 23:30:21 -0300

    banner-feito-iniico

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var typed = new Typed(".auto-type", {
+  strings: ["Front-end", "Criativo", "Curioso", "Diligente"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+
+const pre = document.querySelector(".card-3d-inicio");
+
+document.addEventListener("mousemove", (e) => {
+  rotateElement(e, pre);
+});
+
+function rotateElement(event, element) {
+  // get mouse position
+  const x = event.clientX;
+  const y = event.clientY;
+  // console.log(x, y)
+
+  // find the middle
+  const middleX = window.innerWidth / 2;
+  const middleY = window.innerHeight / 2;
+  // console.log(middleX, middleY)
+
+  // get offset from middle as a percentage
+  // and tone it down a little
+  const offsetX = ((x - middleX) / middleX) * 45;
+  const offsetY = ((y - middleY) / middleY) * 45;
+  // console.log(offsetX, offsetY);
+
+  // set rotation
+  element.style.setProperty("--rotateX", offsetX + "deg");
+  element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+}
